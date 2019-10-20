8952c462847033e4ac17c6bda3a63a67c45ac288
ninalazar <nina_lazar@epam.com>
2019-10-21 00:32:58 +0300

    update log4j.properties

--- src/test/java/com/epam/mentoring/driver/WebDriverInstance.java
+++ src/test/java/com/epam/mentoring/driver/WebDriverInstance.java
@@ -1,6 +1,8 @@
 package com.epam.mentoring.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -15,6 +17,8 @@ public class WebDriverInstance {
         private static ChromeOptions options = new ChromeOptions();
         private static DesiredCapabilities dc = DesiredCapabilities.chrome();
 
+        private static final Logger LOGGER = LogManager.getLogger(WebDriverInstance.class);
+
         private WebDriverInstance(){}
 
         public static WebDriver getDriverInstance(){
@@ -23,6 +27,7 @@ public class WebDriverInstance {
             dc.setCapability(ChromeOptions.CAPABILITY, options);
             try {
                 driver = new RemoteWebDriver(new URL("http://localhost:4450/wd/hub"), dc);
+                LOGGER.info("Driver initialized");
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -33,5 +38,6 @@ public class WebDriverInstance {
     public static void closeDriver(){
         driver.quit();
         driver = null;
+        LOGGER.info("Driver quite successfull");
     }
 }
--- src/test/java/com/epam/mentoring/test/BaseTest.java
+++ src/test/java/com/epam/mentoring/test/BaseTest.java
@@ -3,19 +3,31 @@ package com.epam.mentoring.test;
 import com.epam.mentoring.driver.WebDriverInstance;
 import com.epam.mentoring.testdata.TestData;
 import com.epam.mentoring.testdata.bean.User;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
 
+import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 public abstract class BaseTest {
 
     WebDriver driver;
     User testUser;
+    Logger LOGGER = LogManager.getLogger(BaseTest.class);
+
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite() {
+        PropertyConfigurator.configure(Paths.get(System.getProperty("user.dir") , "src", "test", "resources", "log4j.properties").toAbsolutePath().toString());
+    }
 
     @BeforeClass
     protected void initTest() {
+        LOGGER.info("Initializing ChromeDriver");
         driver = WebDriverInstance.getDriverInstance();
         driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
